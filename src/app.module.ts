c87d772df9929fa44795623b6125625af8da7565
alvaromoca1 <tunime.1@gmail.com>
2021-07-31 23:56:59 -0500

    agregando la configuraion del sentri

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
-import { Module } from '@nestjs/common';
+import { HttpException, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { RavenInterceptor, RavenModule } from 'nest-raven';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    RavenModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR, // Designar como interceptor global
+      useValue: new RavenInterceptor({
+        filters: [
+          {
+            type: HttpException,
+            // Filtrar excepciones de tipo HttpException.
+            // Ignore aquellos que tienen un código de estado de menos de 500
+            filter: (exception: HttpException) => {
+              return 500 > exception.getStatus();
+            },
+          },
+        ],
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { init as SentryInit } from '@sentry/node';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  SentryInit({
+    dsn: 'https://e3441437567743e6b65edde72b6ff1d8@o575899.ingest.sentry.io/5887418',
+  });
   await app.listen(3000);
 }
 bootstrap();
